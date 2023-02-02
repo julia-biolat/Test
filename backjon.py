3f2122bf1138ffbe68bebc56f3582ce33fc017d2
juliabiolata <ljyun7@naver.com>
2023-02-02 17:27:06 +0900

    Create README.md

--- backjon.py
+++
@@ -1,9 +0,0 @@
-import sys
-from collections import deque
-
-N = int(sys.stdin.readline())
-
-for i in range(N):
-    a, b = map(int, sys.stdin.readline().split())
-    c = deque(map(int,sys.stdin.readline().split()))
-
